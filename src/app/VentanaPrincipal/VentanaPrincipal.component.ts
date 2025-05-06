e72f00d5ca9d8a5eefe32e03a5f1cb95b4dc36b3
mnorenac <mnorenac@emergiacc.com>
2025-05-06 16:05:46 -0500

    se crea la estructura y la ventana principal

---
+++ src/app/VentanaPrincipal/VentanaPrincipal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-VentanaPrincipal',
+  imports: [],
+  templateUrl: './VentanaPrincipal.component.html',
+  styleUrl: './VentanaPrincipal.component.css'
+})
+
+export class VentanaPrincipalComponentComponent {
+
+}
