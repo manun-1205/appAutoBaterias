0cfc9da6f04de7dfacc635f66c593875f1c5dbb0
mnorenac <mnorenac@emergiacc.com>
2025-05-12 15:15:35 -0500

    se crean los componentes para carro y moto

---
+++ src/app/models/motos/motos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motos',
+  imports: [],
+  templateUrl: './motos.component.html',
+  styleUrl: './motos.component.css'
+})
+export class MotosComponent {
+
+}
